4ab75c6caa49ce1cbc6827870be4684bca21ce0f
Kevin Moses <kmoses28@gmail.com>
2016-02-24 21:36:51 -0500

    Create main function

--- fuzz.py
+++ fuzz.py
@@ -2,21 +2,26 @@ import requests
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description="Usage: fuzz [discover | test] url OPTIONS")
+def main():
+    parser = argparse.ArgumentParser(description="Usage: fuzz [discover | test] url OPTIONS")
 
-parser.add_argument("--common-words", dest="common_words", metavar="FILE", help="Newline-delimited file of common " +
-    "words to be used in page guessing and input guessing. Required.")
+    parser.add_argument("--common-words", dest="common_words", metavar="FILE", help="Newline-delimited file of common " +
+        "words to be used in page guessing and input guessing. Required.")
 
-if len(sys.argv) < 4 :
-    parser.error("Received incorrect number of arguments")
+    if len(sys.argv) < 4 :
+        parser.error("Received incorrect number of arguments")
 
-else:
-    requestedAction = sys.argv[1]
-    url = sys.argv[2]
+    else:
+        requestedAction = sys.argv[1]
+        url = sys.argv[2]
 
-    if requestedAction == "discover" :
-        #Probably have the discover functionality in its own file called discover.py
-        print "stuff"
+        if requestedAction == "discover" :
+            #Probably have the discover functionality in its own file called discover.py
+            print "Running discovery on '{}'".format(url)
 
-    else :
-        parser.error("Invalid action requested")
+        else :
+            parser.error("Invalid action requested")
+
+
+if __name__ == "__main__":
+    main()
