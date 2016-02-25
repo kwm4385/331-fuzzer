1d8ed11608fd540568370672dde2abb63688ebe7
Kevin Moses <kmoses28@gmail.com>
2016-02-24 22:57:42 -0500

    Should have been using the session

--- fuzz.py
+++ fuzz.py
@@ -22,32 +22,25 @@ def main():
             #Probably have the discover functionality in its own file called discover.py
             print "Running discovery on '{}'".format(url)
             session = tryAuthenticate(url)
+            session.get(url + "login.php")
 
         else :
             parser.error("Invalid action requested")
 
+# Loads the login form for DVWA and tries to log in
 def tryAuthenticate(url):
     s = requests.Session()
     print "Trying to authenticate to DVWA with default credentials..."
     try:
         requests.utils.add_dict_to_cookiejar(s.cookies, {"security": "low"})
-        loginpage = requests.get(url + "login.php")
-        cookies = loginpage.headers["Set-Cookie"].split(";")
-        for c in cookies:
-            if c.startswith("PHPSESSID"):
-                sessid = c.split("=")[1]
-        requests.utils.add_dict_to_cookiejar(s.cookies, {"PHPSESSID": sessid})
-        print "Found session ID token"
-
+        loginpage = s.get(url + "login.php")
         soup = bs(loginpage.content)
         token = soup.body.find('input', attrs={"type": "hidden", "name": "user_token"}).get('value').encode('ascii','ignore')
         if token:
             print "Found CSRF token"
-            # print {"username": "admin", "password": "password", "Login": "Login", "user_token": token}
+
         r = s.post(url + "login.php", data={"username": "admin", "password": "password", "Login": "Login", "user_token": token})
-
         print "Successfully logged in!"
-
     except:
         print "Authentication failed!"
 
