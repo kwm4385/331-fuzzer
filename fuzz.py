6fd00be5789319d9fbe5a8fc5ded2c3b6d6bf7eb
JeremyVargas <jxv6455@rit.edu>
2016-02-24 20:26:42 -0500

    Initial commit of main fuzz module

---
+++ fuzz.py
@@ -0,0 +1,22 @@
+import requests
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Usage: fuzz [discover | test] url OPTIONS")
+
+parser.add_argument("--common-words", dest="common_words", metavar="FILE", help="Newline-delimited file of common " +
+    "words to be used in page guessing and input guessing. Required.")
+
+if len(sys.argv) < 4 :
+    parser.error("Received incorrect number of arguments")
+
+else:
+    requestedAction = sys.argv[1]
+    url = sys.argv[2]
+
+    if requestedAction == "discover" :
+        #Probably have the discover functionality in its own file called discover.py
+
+
+    else :
+        parser.error("Invalid action requested")
