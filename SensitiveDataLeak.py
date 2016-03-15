0e365e75320759536b89797e8906d5ebbe6c7674
King-Ob <oxi8927@g.rit.edu>
2016-03-14 20:19:51 -0400

    sensitive

--- SensitiveDataLeak.py
+++ SensitiveDataLeak.py
@@ -6,18 +6,18 @@ class tactfulDataLeak():
         
         for item in tactful:
             if item in response.text:
-                logger.info("Page: %s\n  Form: %s\n  Vector: %s\n  Status:Code: %s\n" (url, form, vector, response.status_code))
+                logger.info("leaf: %s\n  Form: %s\n  Vector: %s\n  Status:Code: %s\n" (url, form, vector, response.status_code))
                 
                 
-    def execute(self, pages, session, tactics):
+    def execute(self, leafs, session, tactics):
         
         vectors = tactics._getVectors()
         
         logger.info("" + tactics.options.random)
         
-        for page in pages:
-            forms = page.get("inputs").get("forms")
-            url = page.get("url")
+        for leaf in leafs:
+            forms = leaf.get("inputs").get("forms")
+            url = leaf.get("url")
         
             if tactics.options.random == "False" or tactics.options.random == "false":   
                 for form in forms:
